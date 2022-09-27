b5c925798ce27f495b99c818bd1721bb90c42290
x <exit>
2022-09-27 21:48:56 +0000

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,30 @@
-/* eslint-disable import/order */
-/* eslint-disable import/first */
-/* eslint-disable import/newline-after-import */
-/* eslint-disable no-unused-vars */
-/* eslint-disable global-require */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const WebpackLighthousePlugin = require('webpack-lighthouse-plugin');
-// import { WebpackLighthousePlugin } from 'webpack-lighthouse-plugin';
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
   devServer: {
     static: './dist',
-    hot: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-    // new WebpackLighthousePlugin({
-    //   url: 'http://localhost:8080/webpack-dev-server/',
-    // }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
   },
-
-  mode: 'development',
   module: {
     rules: [
       {
-        test: [/\.css$/i, /\.(scss)$/],
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: () => [
-                  require('autoprefixer'),
-                ],
-              },
-            },
-          },
-          {
-            loader: 'sass-loader',
-          },
-        ],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
-
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-        dependency: { not: ['url'] },
-      },
-
     ],
   },
-
 };
